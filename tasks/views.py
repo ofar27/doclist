b28d87489023bc5624d6e5a1041b8d03ce0579cb
ofar <ouissal.farih27@gmail.com>
2024-10-09 17:59:51 +0200

    ajouter une tache

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,9 @@
+from xml.sax.saxutils import escape
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.utils.html import escape
+
 from tasks.models import Collection
 
 # Create your views here.
@@ -10,7 +14,7 @@ def index(request):
 
 def add_collection(request):
     if request.method == "POST":
-        collection_name = request.POST.get("collection-name")
+        collection_name = escape(request.POST.get("collection-name"))
         if collection_name:  # Vérifie si le nom n'est pas vide
             Collection.objects.create(name=collection_name)
     return redirect('home')
